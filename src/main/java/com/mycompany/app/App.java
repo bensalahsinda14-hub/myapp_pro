a761d62c73d3408aaebb6c15615633182f0529e0
Sinda <bensalahsinda14@example.com>
2025-11-15 06:02:17 -0800

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, MyApp Pro!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
